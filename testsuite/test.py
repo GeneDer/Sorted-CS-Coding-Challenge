c55d2f1216297ee0733691eb3289b213d9af7664
Gene Der Su <e870252314@yahoo.com.tw>
2017-04-27 14:48:44 -0700

    test: added some simple teatsuite
    added testsuite for testing.

--- testsuite/test.py
+++ testsuite/test.py
@@ -3,10 +3,15 @@ import os
 
 cwd = os.getcwd()
 for i in os.listdir(cwd + "/testsuite/input"):
-    file_name = i.split('.')
-    if len(file_name) == 2 and file_name[1] == "txt":
+    if i[-3:] == "txt":
         call(["python",
               cwd + "/listSorting.py",
               cwd + "/testsuite/input/" + i,
-              cwd + "/testsuite/expected_output/" + file_name[0] + ".txt"])
-
+              cwd + "/testsuite/actual_output/" + i])
+        with open(cwd + "/testsuite/expected_output/" + i, "r") as eo,\
+             open(cwd + "/testsuite/actual_output/" + i, "r") as ao:
+            test_case = i.split('.')[0]
+            if eo.readline() == ao.readline():
+                print test_case, "Passed! :)"
+            else:
+                print test_case, "Failed! :("
