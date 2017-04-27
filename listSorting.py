97c6c9931d867065d9db6a14fcb9376b41d8dc2d
Gene Der Su <e870252314@yahoo.com.tw>
2017-04-27 13:16:32 -0700

    refactor: initialize repostory
    Added the problem statement, example test case, and some starter code

---
+++ listSorting.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+def my_list_sort(l):
+    pass
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    input_file = args[1]
+    output_file = args[2]
+
+    print input_file, output_file
